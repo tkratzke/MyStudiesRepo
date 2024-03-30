17a8a3aeee8ccbc6273e541a91b24b896d1364b5
Thomas Kratzke <tkratzke@gmail.com>
2024-03-30 07:42:47 -0500

    Rearranging data files.

--- flashCardsGame/Data/FlashCardsGame.bat
+++ flashCardsGame/Data/FlashCardsGame.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
 dir
-java -jar "%ProgramW6432%\TMK-Skagit\FlashCardsGame\FlashCardsGame.jar" Vietnamese-01
+java -jar "%ProgramW6432%\TMK-Skagit\FlashCardsGame\FlashCardsGame.jar" VN-00
 pause
