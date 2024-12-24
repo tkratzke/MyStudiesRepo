e815c0be9d35d8ba20056e571f9606ff55afa483
Thomas Kratzke <tkratzke@gmail.com>
2024-12-24 16:25:03 -0600

    Re-write of markup processor.

--- myStudies/src/main/java/com/skagit/paragraphFlattener/ParagraphFlattener.java
+++ myStudies/src/main/java/com/skagit/paragraphFlattener/ParagraphFlattener.java
@@ -8,140 +8,105 @@ import java.io.PrintWriter;
 
 public class ParagraphFlattener {
 
+    static private enum BufferType {
+	UNKNOWN(false, ""), BODY(false, "\n\n"), NEW_FOOTNOTE(true, "\n"), CONTINUE_FOOTNOTE(true, "\n");
+
+	final boolean _isPartOfFootnote;
+	final String _prelim;
+
+	BufferType(final boolean isPartOfFootnote, final String prelim) {
+	    _isPartOfFootnote = isPartOfFootnote;
+	    _prelim = prelim;
+	}
+    };
+
+    static final String _NewFootnoteMarkup = "(Footnote";
+    static final String _ContinueFootnoteMarkup = "(Continue Footnote";
+    static final String _NewFootnoteMarkupLc = _NewFootnoteMarkup.toLowerCase();
+    static final String _ContinueFootnoteMarkupLc = _ContinueFootnoteMarkup.toLowerCase();
+
     public static void main(final String[] args) {
-	final File fIn = new File("../GiBill/Part1.txt");
-	final File fOut = new File("../GiBill/Part1-Out.txt");
-	String activeLine = "";
-	boolean onFootnote = false;
-	int nFootnoteLinesPrinted = 0;
+	// final File fIn = new File("../GiBill/TestFile.txt");
+	// final File fOut = new File("../GiBill/Processed TestFile.txt");
+	final File fIn = new File("../GiBill/GI Bill SourceText.txt");
+	final File fOut = new File("../GiBill/Processed GI Bill SourceText.txt");
+	String buffer = "";
 	boolean havePrinted = false;
+	BufferType bufferType = BufferType.UNKNOWN;
 	try (final FileReader fileReader = new FileReader(fIn);
 		final BufferedReader bufferedReader = new BufferedReader(fileReader);
 		final PrintWriter pw = new PrintWriter(fOut)) {
 	    for (;;) {
 		try {
 		    final String line = bufferedReader.readLine().trim();
-		    final boolean emptyLine = line.length() == 0;
+		    /** An empty line dumps the buffer and sets bufferType to UNKNOWN. */
+		    if (line.length() == 0) {
+			dumpBuffer(pw, havePrinted, buffer, bufferType);
+			if (buffer.length() > 0) {
+			    havePrinted = true;
+			    buffer = "";
+			    bufferType = BufferType.UNKNOWN;
+			}
+			continue;
+		    }
+		    /** It's not an empty line. */
 		    final String lineLc = line.toLowerCase();
-		    final boolean startsFootnote = lineLc.startsWith("(footnote");
-		    final boolean endsFootnote = lineLc.endsWith("footnote)");
-		    final boolean wasOnFootnote = onFootnote;
-		    if (emptyLine) {
-			/** Wrap up the previous line and print it if it's not empty. */
-			activeLine = activeLine.replaceAll("  ", " ");
-			if (activeLine.length() > 0) {
-			    final String format;
-			    if (!havePrinted) {
-				format = "%s";
-				havePrinted = true;
-			    } else if (onFootnote) {
-				if (nFootnoteLinesPrinted == 0) {
-				    format = "\n%s";
-				} else {
-				    format = "\n\t%s";
-				}
-				++nFootnoteLinesPrinted;
-			    } else {
-				format = "\n\n%s";
-			    }
-			    pw.printf(format, activeLine);
-			    System.out.printf(format, activeLine);
-			    activeLine = "";
-			}
-			continue;
+		    final boolean bufferIsFootnote = bufferType._isPartOfFootnote;
+		    final boolean lineStartsFootnote = lineLc.startsWith(_NewFootnoteMarkupLc);
+		    final boolean lineStartsFootnoteContinuation;
+		    if (!lineLc.startsWith(_ContinueFootnoteMarkupLc)) {
+			lineStartsFootnoteContinuation = false;
+		    } else if (!bufferIsFootnote) {
+			lineStartsFootnoteContinuation = false;
+		    } else {
+			lineStartsFootnoteContinuation = true;
 		    }
-		    if (!wasOnFootnote) {
-			if (!startsFootnote) {
-			    /** Just add to the line. */
-			    activeLine += " " + line;
-			} else {
-			    /** We were on body, and switched to footnote. Must print out body. */
-			    activeLine = activeLine.replaceAll("  ", " ");
-			    if (activeLine.length() > 0) {
-				final String format;
-				if (!havePrinted) {
-				    format = "%s";
-				    havePrinted = true;
-				} else {
-				    format = "\n\n%s";
-				}
-				pw.printf(format, activeLine);
-				System.out.printf(format, activeLine);
-			    }
-			    /** We're now on a footnote. */
-			    activeLine = line;
-			    onFootnote = true;
-			    nFootnoteLinesPrinted = 0;
-			    if (endsFootnote) {
-				/** We started and ended the footnote with line. Print the footnote. */
-				activeLine = activeLine.replaceAll("  ", " ");
-				final String format;
-				if (!havePrinted) {
-				    format = "%s";
-				    havePrinted = true;
-				} else {
-				    format = "\n%s";
-				}
-				pw.printf(format, activeLine);
-				System.out.printf(format, activeLine);
-				activeLine = "";
-				onFootnote = false;
-			    }
+		    if (lineStartsFootnote) {
+			dumpBuffer(pw, havePrinted, buffer, bufferType);
+			havePrinted = buffer.length() > 0;
+			buffer = line;
+			bufferType = BufferType.NEW_FOOTNOTE;
+		    } else if (lineStartsFootnoteContinuation) {
+			dumpBuffer(pw, havePrinted, buffer, bufferType);
+			havePrinted = buffer.length() > 0;
+			buffer = line.substring(_ContinueFootnoteMarkup.length()).trim();
+			if (buffer.charAt(0) == ':') {
+			    buffer = buffer.substring(1);
 			}
-			continue;
-		    }
-		    if (wasOnFootnote) {
-			activeLine += " " + line;
-			if (endsFootnote) {
-			    activeLine = activeLine.replaceAll("  ", " ");
-			    final String format;
-			    if (!havePrinted) {
-				format = "%s";
-				havePrinted = true;
-			    } else {
-				if (nFootnoteLinesPrinted == 0) {
-				    format = "\n%s";
-				} else {
-				    format = "\n\t%s";
-				}
-			    }
-			    pw.printf(format, activeLine);
-			    System.out.printf(format, activeLine);
-			    activeLine = "";
-			    onFootnote = false;
-			    nFootnoteLinesPrinted = 0;
-			    continue;
-			} else {
-			    /**
-			     * We were on a footnote and did not end it. We've already added line to the
-			     * activeLine so there's nothing to do.
-			     */
-			}
+			bufferType = BufferType.CONTINUE_FOOTNOTE;
+		    } else if (bufferType == BufferType.UNKNOWN) {
+			bufferType = BufferType.BODY;
+			buffer = line;
+		    } else {
+			buffer = buffer + " " + line;
 		    }
 		} catch (final IOException | NullPointerException e) {
-		    if (activeLine.length() > 0) {
-			activeLine = activeLine.replaceAll("  ", " ");
-			final String format;
-			if (!havePrinted) {
-			    format = "%s";
-			    havePrinted = true;
-			} else if (onFootnote) {
-			    if (nFootnoteLinesPrinted == 0) {
-				format = "\n%s";
-			    } else {
-				format = "\n\t%s";
-			    }
-			} else {
-			    format = "\n\n%s";
-			}
-			pw.printf(format, activeLine);
-			System.out.printf(format, activeLine);
-		    }
+		    dumpBuffer(pw, havePrinted, buffer, bufferType);
 		    break;
 		}
 	    }
 	} catch (final IOException e) {
 	}
+    }
+
+    private static boolean dumpBuffer(final PrintWriter pw, final boolean havePrinted, final String buffer0,
+	    final BufferType bufferType) {
+	final String buffer = buffer0.trim().replaceAll("  ", " ");
+	if (buffer.length() == 0) {
+	    return /* dumpedSomething= */false;
+	}
+	final String prelim = havePrinted ? bufferType._prelim : "";
+	final String format0;
+	if (bufferType == BufferType.BODY) {
+	    format0 = "%s";
+	} else if (bufferType == BufferType.NEW_FOOTNOTE) {
+	    format0 = "%s";
+	} else {
+	    format0 = "\t%s";
+	}
+	pw.printf("%s" + format0, prelim, buffer);
+	System.out.printf("%s" + format0, prelim, buffer);
+	return /* dumpedSomething= */true;
     }
 
 }
